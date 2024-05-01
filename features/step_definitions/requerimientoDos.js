b33fabfe501caa6011073f7459c72d5fc46a51ee
Dionrvelarde <dion.rizo@iteso.mx>
2024-05-01 17:54:47 -0600

    movimiento final del requerimiento 2

--- features/step_definitions/requerimientoDos.js
+++ features/step_definitions/requerimientoDos.js
@@ -1,3 +1,106 @@
 const assert = require("assert");
 const { Given, When, Then } = require("@cucumber/cucumber");
 
+//------------------------------1-----------------------------------------------------
+Given('el usuario quiere poner una calificacion en especifico', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('El usuario va a poner una calificacion de ocho punto cinco', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('el sistema deja poner la calificaion que quiere el usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+//------------------------------2-----------------------------------------------------
+
+Given('el usuario quiere ingresar manualmente una calificación', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('el usuario ingresa la calificación de {float} en el campo de entrada de texto', function (float) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('el sistema registra la calificación ingresada por el usuario para evaluar al profesor específico', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+//------------------------------3-----------------------------------------------------
+
+Given('el usuario quiere poner una calificación en especifico', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('el usuario intenta poner una calificación de once punto tres', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('el sistema muestra un mensaje de error indicando que la calificación está fuera del rango permitido', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+//------------------------------4-----------------------------------------------------
+
+Given('el usuario quiere ingresar una calificación', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('el usuario intenta ingresar la calificación {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('el sistema muestra un mensaje de error solicitando al estudiante que ingrese un valor numérico válido dentro del rango de {int} a {int} con un decimal', function (int, int2) {
+    // Then('el sistema muestra un mensaje de error solicitando al estudiante que ingrese un valor numérico válido dentro del rango de {int} a {float} con un decimal', function (int, float) {
+    // Then('el sistema muestra un mensaje de error solicitando al estudiante que ingrese un valor numérico válido dentro del rango de {float} a {int} con un decimal', function (float, int) {
+    // Then('el sistema muestra un mensaje de error solicitando al estudiante que ingrese un valor numérico válido dentro del rango de {float} a {float} con un decimal', function (float, float2) {
+      // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+//------------------------------5-----------------------------------------------------
+
+Given('el usuario quiere evaluar a un profesor', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('el usuario envía una calificación válida de {float}', function (float) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('el sistema actualiza automáticamente el promedio de calificaciones del profesor', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+//------------------------------6-----------------------------------------------------
+
+Given('el usuario quiere registrar una calificación para un profesor', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('el usuario ingresa una calificación de {float} para el profesor', function (float) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('el sistema registra la calificación del estudiante para el profesor y la muestra en el perfil del profesor para futuras referencias', function () {   
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
